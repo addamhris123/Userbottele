c30f6de1c7f11aaa225ef92c516ab17494a06380
Damz Developer <119148434+addamhris123@users.noreply.github.com>
2024-07-30 16:44:06 +0800

    Update UserBot.py

--- UserBot.py
+++ UserBot.py
@@ -20,7 +20,7 @@ os.makedirs(QR_CODE_DIR, exist_ok=True)
 blacklisted_groups = []
 
 # Watermark text
-WATERMARK_TEXT = "sc script github.com/addamhris123/Userbottele"
+WATERMARK_TEXT = "💰"
 
 # Function to append watermark to a message
 def append_watermark_to_message(message):
